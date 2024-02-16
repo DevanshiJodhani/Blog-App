75cf665b200b4319d7dc177ccc80ebe1a715c698
DevanshiJodhani <devanshijodhani45@gmail.com>
2024-02-16 14:03:55 +0530

    uploded video file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,40 +7,40 @@ import PostCard from '../Components/PostCard'
 
 function Home() {
 
-    const [posts, setPosts] = useState([])
-  
-    useEffect(() => {
-      appwriteservice.getPosts([]).then((posts) => {
-        if (posts) {
-          setPosts(posts.documents)
-        }
-      })
-    }, [])
-    if (posts.length === 0) {
-      return (
-        <div className='w-full py-8'>
-        <Container>
-          <div className="flex flex-wrap">
-            <h1>Login to read posts</h1>
-          </div>
-        </Container>
-      </div>
-      )
-    }
-  
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    appwriteservice.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents)
+      }
+    })
+  }, [])
+  if (posts.length === 0) {
     return (
       <div className='w-full py-8'>
         <Container>
           <div className="flex flex-wrap">
-            {posts.map((post) => (
-              <div className="p-2 w-1/4" key={post.$id}>
-                <PostCard {...post} />
-              </div>
-            ))}
+            <h1 className="text-white">Login to read posts</h1>
           </div>
         </Container>
       </div>
     )
   }
-  
-  export default Home
+
+  return (
+    <div className='w-full py-8'>
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Home
